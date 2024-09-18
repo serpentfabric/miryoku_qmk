76124a3c7613fadeda25aaf42851f9b891c58efd
chad <41795094+serpentfabric@users.noreply.github.com>
2024-09-18 10:07:39 -0400

    remove permissive hold for now

--- users/manna-harbour_miryoku/config.h
+++ users/manna-harbour_miryoku/config.h
@@ -11,8 +11,6 @@
 #undef TAPPING_TERM
 #define TAPPING_TERM 200
 
-#define PERMISSIVE_HOLD
-
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 0
 
